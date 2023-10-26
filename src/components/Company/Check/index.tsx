f8bcb13638e6f70b180cc640b68d2cccc20c8116
sinchangho <vkdghckdh@gmail.com>
2023-10-26 20:28:56 +0900

    feat: 구인처(회사)전용 입력 템플릿 생성

---
+++ src/components/Company/Check/index.tsx
@@ -0,0 +1,295 @@
+"use client";
+import { useState } from "react";
+import { RadioGroup } from "@headlessui/react";
+import { CheckCircleIcon, TrashIcon } from "@heroicons/react/20/solid";
+import BusinessNumberInput from "../Template/BusinessNumberInput";
+import BusinessYearInput from "../Template/BusinessYearInput";
+import CompanyNameAndCEONameInput from "../Template/CompanyNameInput";
+import PhoneInput from "@/components/Template/PhoneInput";
+import JobCodeInput from "../Template/JobCodeInput";
+import FaxNumberInput from "../Template/FaxNumberInput";
+import CeoNameInput from "../Template/CEONameInput";
+import DoubleCheckButton from "@/components/Template/DoubleCheck";
+
+const paymentMethods = [
+  { id: "credit-card", title: "Credit card" },
+  { id: "paypal", title: "PayPal" },
+  { id: "etransfer", title: "eTransfer" },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function CompanyRegister() {
+  const [selectedDeliveryMethod, setSelectedDeliveryMethod] = useState();
+
+  return (
+    <div className="bg-gray-50">
+      <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="sr-only">구인처 등록</h2>
+
+        <form className="lg:grid lg:gap-x-12 xl:gap-x-16">
+          <div>
+            <div>
+              <h1 className="text-4xl	font-medium text-gray-900">구인처 등록</h1>
+
+              <div className="mt-20">
+                <h2 className="text-lg font-medium text-gray-900">기업정보</h2>
+                <div className="mt-4 grid grid-cols-3 gap-y-6 sm:grid-cols-3 sm:gap-x-4">
+                  <BusinessYearInput></BusinessYearInput>
+                  <BusinessNumberInput></BusinessNumberInput>
+                  {/* 중복 확인 버튼 */}
+                  <div className="mt-6 flex justify-between">
+                    <DoubleCheckButton></DoubleCheckButton>
+                  </div>
+                </div>
+                <div className="mt-4 grid grid-cols-3 gap-y-6 sm:grid-cols-3 sm:gap-x-4">
+                  <CompanyNameAndCEONameInput></CompanyNameAndCEONameInput>
+                  <CeoNameInput></CeoNameInput>
+                </div>
+
+                <div className="mt-4">
+                  <JobCodeInput></JobCodeInput>
+                </div>
+                <div className="mt-4">
+                  <PhoneInput></PhoneInput>
+                  <FaxNumberInput></FaxNumberInput>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-10 border-t border-gray-200 pt-10">
+              <div className="mt-10">
+                <h2 className="text-lg font-medium text-gray-900">상세정보</h2>
+              </div>
+              
+
+              <div className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
+                <div>
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    담당자 명
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="first-name"
+                      name="first-name"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    이메일
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="last-name"
+                      name="last-name"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="company"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    기업소재지
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="company"
+                      id="company"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="address"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    기업설립연도
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="address"
+                      id="address"
+                      autoComplete="street-address"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <fieldset className="mt-4">
+                <h2 className="text-lg font-medium text-gray-900">4대보험가입</h2>
+
+                <legend className="sr-only">Payment type</legend>
+                <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
+                  {paymentMethods.map((paymentMethod, paymentMethodIdx) => (
+                    <div key={paymentMethod.id} className="flex items-center">
+                      {paymentMethodIdx === 0 ? (
+                        <input
+                          id={paymentMethod.id}
+                          name="payment-type"
+                          type="radio"
+                          defaultChecked
+                          className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                      ) : (
+                        <input
+                          id={paymentMethod.id}
+                          name="payment-type"
+                          type="radio"
+                          className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                        />
+                      )}
+
+                      <label
+                        htmlFor={paymentMethod.id}
+                        className="ml-3 block text-sm font-medium text-gray-700"
+                      >
+                        {paymentMethod.title}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </fieldset>
+
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="apartment"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    사업자등록증
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="apartment"
+                      id="apartment"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="city"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    주요사업
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="city"
+                      id="city"
+                      autoComplete="address-level2"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Payment */}
+            <div className="mt-10 border-t border-gray-200 pt-10">
+              <h2 className="text-lg font-medium text-gray-900">구인내용</h2>
+
+              
+
+              <div className="mt-6 grid grid-cols-4 gap-x-4 gap-y-6">
+                <div className="col-span-4">
+                  <label
+                    htmlFor="card-number"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    구인직무1
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="card-number"
+                      name="card-number"
+                      autoComplete="cc-number"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-span-4">
+                  <label
+                    htmlFor="name-on-card"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    구인인원
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      id="name-on-card"
+                      name="name-on-card"
+                      autoComplete="cc-name"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div className="col-span-3">
+                  <label
+                    htmlFor="expiration-date"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    급여
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="expiration-date"
+                      id="expiration-date"
+                      autoComplete="cc-exp"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="cvc"
+                    className="block text-sm font-medium text-gray-700"
+                  >
+                    급무형태
+                  </label>
+                  <div className="mt-1">
+                    <input
+                      type="text"
+                      name="cvc"
+                      id="cvc"
+                      autoComplete="csc"
+                      className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Company/Template/FaxNumberInput.tsx
@@ -0,0 +1,40 @@
+export default function FaxNumberInput() {
+    return (
+      <div>
+        <label
+          htmlFor="fax-number"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          팩스번호(선택)
+        </label>
+        <div className="w-1/2 relative mt-2 rounded-md shadow-sm">
+          <div className="mt-1 flex justify-between">
+            <input
+              type="number"
+              id="fax-number-1"
+              name="fax-number-1"
+              autoComplete="number"
+              className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            />
+            <div className="w-auto flex items-center justify-center mx-2">-</div>
+            <input
+              type="number"
+              id="fax-number-2"
+              name="fax-number-2"
+              autoComplete="number"
+              className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            />
+            <div className="w-auto flex items-center justify-center mx-2">-</div>
+            <input
+              type="number"
+              id="fax-number-3"
+              name="fax-number-3"
+              autoComplete="number"
+              className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
