be3ebd470353b1bbd0351fa87b03d5270962c1f8
sinchangho <vkdghckdh@gmail.com>
2023-10-26 16:44:42 +0900

    feat: 사이드바 생성

--- src/app/company/page.tsx
+++ src/app/company/page.tsx
@@ -1,5 +1,18 @@
-export default function Company(){
-    return <>
-      Company!!
-    </>
-  }
+import Example from "@/components/Company/Check";
+import Sidebar from "@/components/Sidebar";
+import SidebarNav from "@/components/SidebarNav";
+import '@/styles/globals.css';
+
+export default function Company({}) {
+  return (
+    <div className="appContainer">
+      <div className="sidebarContent">
+      <SidebarNav />
+      </div>
+      <div className="pageContent">
+        <Example></Example>
+        {/* <Component {...pageProps} /> */}
+      </div>
+    </div>
+  );
+}
