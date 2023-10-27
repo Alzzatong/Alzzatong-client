91fe5b0c7b54d0c0fca9e41193862d1d579c4170
sinchangho <vkdghckdh@gmail.com>
2023-10-28 00:30:49 +0900

    fix: 폴더 및 파일 정리

---
+++ src/components/Template/Input/StringInput.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  id: string;
+}
+
+export default function StringInput({ id }: Props) {
+  return (
+    <div className="mt-1">
+      <input
+        type="text"
+        id={id}
+        name={id}
+        autoComplete="off"
+        className="block w-full p-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+      />
+    </div>
+  );
+}
