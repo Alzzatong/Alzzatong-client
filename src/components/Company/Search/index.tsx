1e6ff9d7dc99b85fe40055934a2835bcc4d4e8e6
sinchangho <vkdghckdh@gmail.com>
2023-10-28 00:32:35 +0900

    fix: 폴더 및 파일 정리

--- src/components/Company/Search/index.tsx
+++ src/components/Company/Search/index.tsx
@@ -5,8 +5,10 @@ import LabelText from "@/components/Template/LabelText/LabelText";
 import YearInput from "@/components/Template/Input/YearInput";
 import NameInput from "@/components/Template/Input/NameInput";
 import SearchButton from "@/components/Template/Button/Search";
+import Link from "next/link";
 const company = [
   {
+    id: 1,
     jobType: "농업업종",
     jobAvailable: "구인중",
     name: "(주)홍길동 주식회사",
@@ -19,6 +21,7 @@ const company = [
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
+    id: 2,
     jobType: "농업업종",
     jobAvailable: "구인중",
     name: "(주)홍길동 주식회사",
@@ -31,6 +34,7 @@ const company = [
       "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   {
+    id: 3,
     jobType: "농업업종",
     jobAvailable: "구인중",
     name: "(주)홍길동 주식회사",
@@ -77,84 +81,88 @@ export default function CompanySearch() {
           <SearchButton></SearchButton>
         </div>
 
-        <h2 className="mt-20 text-xl	font-medium text-gray-900">검색결과</h2>
+        <h2 className="mt-10 text-xl	font-medium text-gray-900">검색결과</h2>
         <div className="min-h-[560px]">
           <ul
             role="list"
-            className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-1"
+            className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-1"
           >
             {company.map((company) => (
               <li
-                key={company.email}
+                key={company.id}
                 className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
               >
-                <div className="flex w-full items-center justify-between space-x-6 p-6">
-                  <div className="flex-1 truncate">
-                    <div className="flex gap-1">
-                      <div className="text-blue-500 text-sm font-medium font-['Pretendard'] leading-tight">
-                        농업업종
-                      </div>
-                      <div className="text-blue-500  text-sm font-medium font-['Pretendard'] leading-tight">
-                        |
-                      </div>
-                      <div className="text-blue-500 text-sm font-medium font-['Pretendard'] leading-tight">
-                        구인중
+                <Link href={`/company/${company.id}`}>
+                  
+                    <div className="flex w-full items-center justify-between space-x-6 p-6 bg-gray-100 hover:bg-white shadow rounded">
+                      <div className="flex-1 truncate">
+                        <div className="flex gap-1">
+                          <div className="text-blue-500 text-sm font-medium font-['Pretendard'] leading-tight">
+                            농업업종
+                          </div>
+                          <div className="text-blue-500  text-sm font-medium font-['Pretendard'] leading-tight">
+                            |
+                          </div>
+                          <div className="text-blue-500 text-sm font-medium font-['Pretendard'] leading-tight">
+                            구인중
+                          </div>
+                        </div>
+                        <div className="mt-1 flex items-center space-x-3">
+                          <div className="text-black text-xl font-bold font-['Pretendard'] leading-7">
+                            {company.name}
+                          </div>
+                          {/* <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                            {company.role}
+                        </span> */}
+                        </div>
+                        <div className="mt-4 flex gap-4">
+                          <div className="w-10 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
+                            <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
+                              번호
+                            </div>
+                          </div>
+                          <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
+                            {company.number}
+                          </div>
+                          <div className="w-20 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
+                            <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
+                              업무 담당자
+                            </div>
+                          </div>
+                          <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
+                            {company.manager}
+                          </div>
+                          <div className="w-24 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
+                            <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
+                              담당자 연락처
+                            </div>
+                          </div>
+                          <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
+                            {company.manager_phone}
+                          </div>
+                        </div>
+                        <div className="mt-2 flex gap-4">
+                          <div className="w-10 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
+                            <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
+                              이메일
+                            </div>
+                          </div>
+                          <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
+                            {company.email}
+                          </div>
+                          <div className="w-10 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
+                            <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
+                              주소
+                            </div>
+                          </div>
+                          <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
+                            {company.address}
+                          </div>
+                        </div>
                       </div>
                     </div>
-                    <div className="mt-1 flex items-center space-x-3">
-                      <div className="text-black text-xl font-bold font-['Pretendard'] leading-7">
-                        {company.name}
-                      </div>
-                      {/* <span className="inline-flex flex-shrink-0 items-center rounded-full bg-green-50 px-1.5 py-0.5 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                        {company.role}
-                      </span> */}
-                    </div>
-                    <div className="mt-4 flex gap-4">
-                      <div className="w-10 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
-                        <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
-                          번호
-                        </div>
-                      </div>
-                      <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
-                        {company.number}
-                      </div>
-                      <div className="w-20 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
-                        <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
-                          업무 담당자
-                        </div>
-                      </div>
-                      <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
-                        {company.manager}
-                      </div>
-                      <div className="w-24 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
-                        <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
-                          담당자 연락처
-                        </div>
-                      </div>
-                      <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
-                        {company.manager_phone}
-                      </div>
-                    </div>
-                    <div className="mt-2 flex gap-4">
-                      <div className="w-10 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
-                        <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
-                          이메일
-                        </div>
-                      </div>
-                      <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
-                        {company.email}
-                      </div>
-                      <div className="w-10 h-6 px-2 py-0.5 bg-neutral-200 bg-opacity-50 rounded justify-center items-center gap-2.5 inline-flex">
-                        <div className="text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
-                          주소
-                        </div>
-                      </div>
-                      <div className="mt-1 text-black text-sm font-medium font-['Pretendard'] leading-tight">
-                        {company.address}
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                  
+                </Link>
               </li>
             ))}
           </ul>
