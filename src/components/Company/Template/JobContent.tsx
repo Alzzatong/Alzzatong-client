77377546f918a41601541f9ddd46311c22dc1ea5
sinchangho <vkdghckdh@gmail.com>
2023-11-04 02:21:02 +0900

    feat: 구인직무 컴포넌트에 맞춰 데이터 저장 하기

--- src/components/Company/Template/JobContent.tsx
+++ src/components/Company/Template/JobContent.tsx
@@ -1,22 +1,43 @@
 import NameInput from "@/components/Template/Input/NameInput";
 import StringInput from "@/components/Template/Input/StringInput";
 import LabelText from "@/components/Template/LabelText/LabelText";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import EmployJobButton from "./EmployJobButton";
-// ... (기타 필요한 import 구문)
-// interface Props {
-//   index: number;
-// }
-interface JobContentProps {
-    index: number;
-    removeItem: (index: number) => void;
-}
+import { RecruitData } from "../Register";
+import NumberInput from "@/components/Template/Input/NumberInput";
 
-function JobContent( props: JobContentProps) {
-    const { index, removeItem } = props;
-    // const removeItem = (index: number) => () => {
-    //     props.removeItem(index);
-    //   };
+interface JobContentProps {
+  index: number;
+  handleJobContent: (index: number, recruit: RecruitData) => void;  
+  removeItem: (index: number) => void;
+}
+export const initialRecruitData: RecruitData = {
+  created_at: new Date(),
+  company_id: 0,
+  job_type: "",
+  number_of_hires: "",
+  salary: "",
+  working_type: "",
+  work_start_hour: "",
+  work_end_hour: "",
+  lunch_hour: "",
+  etc: "",
+  job_availablility: false
+
+};
+function JobContent(props: JobContentProps) {
+  const { index, removeItem,  handleJobContent } = props;
+  const [recruit, setRecruit] = useState<RecruitData>(initialRecruitData);
+  
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const updatedRecruit = {
+      ...recruit,
+      [e.target.id]: e.target.value,
+    };
+    setRecruit(updatedRecruit);
+    handleJobContent(index, updatedRecruit);
+    
+  };
   return (
     <>
       <div className="mt-10 md-4">
@@ -25,39 +46,44 @@ function JobContent( props: JobContentProps) {
 
       <div className="mt-6 grid grid-cols-4 gap-x-4 gap-y-6">
         <div>
-          <LabelText
-            id={`job-type${index + 1}`}
-            text={`구인직무${index + 1}`}
-          />
-          <NameInput id={`job-type${index + 1}`}></NameInput>
+          <LabelText text={`구인직무${index + 1}`} />
+          <NameInput
+            id="job_type"
+            value={recruit.job_type}
+            onChange={handleChange}
+          ></NameInput>
         </div>
         <div>
-          <LabelText id={`NumberOfHires${index + 1}`} text="구인인원" />
-          <NameInput id={`NumberOfHires${index + 1}`}></NameInput>
+          <LabelText text="구인인원" />
+          <NumberInput
+            id="number_of_hires"
+            value={recruit.number_of_hires}
+            onChange={handleChange}
+          ></NumberInput>
         </div>
         <div>
           {/* 급여 */}
-          <LabelText id={`salary${index + 1}`} text="급여" />
-          <NameInput id={`salary${index + 1}`}></NameInput>
+          <LabelText text="급여" />
+          <NameInput id='salary' value={recruit.salary} onChange={handleChange}></NameInput>
         </div>
       </div>
       <div className="mt-6 grid grid-cols-4 gap-x-4 gap-y-6">
         <div>
           {/* 근무형태 */}
-          <LabelText id={`working-type${index + 1}`} text="근무형태" />
-          <NameInput id={`working-type${index + 1}`}></NameInput>
+          <LabelText text="근무형태" />
+          <NameInput id='working_type' value={recruit.working_type} onChange={handleChange}></NameInput>
         </div>
         <div>
           {/* 근무시간 */}
-          <LabelText id={`working-time${index + 1}`} text="근무시간" />
+          <LabelText text="근무시간" />
           <div className="flex gap-3">
             {/* 시작시간, 종료시간 */}
-            <NameInput id={`work-start-hour${index + 1}`}></NameInput>
-            <NameInput id={`work-end-hour${index + 1}`}></NameInput>
+            <NameInput id="work_start_hour" value={recruit.work_start_hour} onChange={handleChange}></NameInput>
+            <NameInput id="work_end_hour" value={recruit.work_end_hour} onChange={handleChange}></NameInput>
           </div>
         </div>
         <div>
-          {/* 주 소정근로시간 계싼 */}
+          {/* 주 소정근로시간 계산 */}
           <div className="mt-6">
             <div className="text-zinc-500 text-sm font-normal font-['Pretendard'] leading-tight">
               주 소정근로시간 : 40시간
@@ -66,13 +92,13 @@ function JobContent( props: JobContentProps) {
         </div>
         <div>
           {/* 점심시간 */}
-          <LabelText id={`lunch-time${index + 1}`} text="점심시간" />
-          <NameInput id={`lunch-time${index + 1}`}></NameInput>
+          <LabelText text="점심시간" />
+          <NameInput id="lunch_hour" value={recruit.lunch_hour} onChange={handleChange}></NameInput>
         </div>
       </div>
       <div className="mt-4">
-        <LabelText id={`etc${index + 1}`} text="비고" />
-        <StringInput id={`etc${index + 1}`} />
+        <LabelText text="비고" />
+        <StringInput id="etc" value={recruit.etc} onChange={handleChange}/>
       </div>
     </>
   );
--- src/components/Template/Button/Add.tsx
+++ src/components/Template/Button/Add.tsx
@@ -1,12 +1,14 @@
-export default function AddButton() {
+interface Props {
+  addItem: () => void;
+}
+export default function AddButton({addItem}: Props) {
   return (
-    <div className="flex justify-end">
-      <button
-        type="button"
-        className="rounded bg-indigo-50 px-2 py-1 text-xs font-semibold text-blue-600 shadow-sm hover:bg-indigo-100"
-      >
-        + 추가하기
-      </button>
-    </div>
+    <button
+      type="button"
+      className="ml-auto rounded bg-indigo-50 px-2 py-1 my-1 text-xs font-semibold text-blue-600 shadow-sm hover:bg-indigo-100"
+      onClick={addItem}
+    >
+      + 추가하기
+    </button>
   );
 }
