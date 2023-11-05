7b6f2f45a116161c8950b79b4f1961b6cdd8c8aa
sinchangho <vkdghckdh@gmail.com>
2023-11-05 18:30:10 +0900

    feat: 상위 컴포넌트인 layout에서 데이터 불러오기

--- src/app/company/[cid]/layout.tsx
+++ src/app/company/[cid]/layout.tsx
@@ -1,31 +1,66 @@
 "use client";
 import CompanyDetail from "@/components/Company/Detail";
-import CompanyConsult from "@/components/Company/Consult";
+import CompanyConsult, { ConsultData } from "@/components/Company/Consult";
 import TabNavigation from "@/components/Company/TabNav/TabNavigation";
-import { useState } from "react";
-
+import { use, useEffect, useState } from "react";
+import Loading from "@/components/Loading";
+import { supabase } from "@/lib/supabase/supabase";
+import { CompanyData, RecruitData, initialCompanyData } from "@/components/Company/Interface/CompanyInterface";
+import { getConsultServerSideProps, getDetailServerSideProps } from "@/services/supabase/companySelect";
+import { get } from "http";
 
 export default function DetailPage(props: any) {
-  const [currentTab, setCurrentTab] = useState("구직자 상세정보"); // 초기 상태를 첫번째 탭으로 설정
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentTab, setCurrentTab] = useState("구직자 상세정보");
+  const [companyData, setCompanyData] = useState<CompanyData>(initialCompanyData);
+  const [recruits, setRecruits] = useState<RecruitData[]>([]);
+  // 상담 리스트를 받아와서 저장합니다.
+  const [consultList, setConsultList] = useState<ConsultData[]>([]);
+  
+  const addRecruit = (recruit : RecruitData) =>{
+    setRecruits([...recruits, recruit]); // 인자로 받은 recruit을 추가합니다.
+  }
+  const addConsult = (consult : ConsultData) =>{
+    setConsultList([...consultList, consult]); // 인자로 받은 consult을 추가합니다.
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      getDetailServerSideProps({id: props.params.cid, setCompanyData: setCompanyData, addRecruit: addRecruit});
+      getConsultServerSideProps({id: props.params.cid, setConsultList: setConsultList});
+      console.log("레이아웃에서 consultList: ", consultList);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
 
   function handleClick(name: string) {
     setCurrentTab(name); // 클릭된 탭으로 currentTab 상태를 변경합니다.
   }
 
-  return (
-    <div className="sidebarContainer">
-      <div className="sidebarContent"></div>
-      <div className="pageContent">
-        <div className="whiteSpace" />
+  if (isLoading) {
+    return <Loading />;
+  } else {
+    return (
+      <div className="sidebarContainer">
+        <div className="sidebarContent"></div>
+        <div className="pageContent">
+          <div className="whiteSpace" />
 
-        <div className=" text-black text-xl font-medium font-['Pretendard'] ">
-          <TabNavigation handleClick={handleClick} currentTab={currentTab} />
+          <div className=" text-black text-xl font-medium font-['Pretendard'] ">
+            <TabNavigation handleClick={handleClick} currentTab={currentTab} />
+          </div>
+          <div className="className= mt-10">
+            {currentTab == "구직자 상세정보" ? (
+              <CompanyDetail companyInfo={companyData} recruitsInfo={recruits}/>
+            ) : (
+              <CompanyConsult company_id={props.params.cid} consult_list={consultList}/>
+            )}
+          </div>
         </div>
-        <div className="className= mt-10">
-          {currentTab=="구직자 상세정보" ? <CompanyDetail/>:<CompanyConsult/>}</div>
+        <div className="sidebarContent"></div>
       </div>
-      <div className="sidebarContent"></div>
-    </div>
-
-  );
+    );
+  }
 }
---
+++ src/services/supabase/companySelect.tsx
@@ -0,0 +1,52 @@
+import { ConsultData } from "@/components/Company/Consult";
+import {
+  CompanyData,
+  RecruitData,
+} from "@/components/Company/Interface/CompanyInterface";
+import { supabase } from "@/lib/supabase/supabase";
+
+interface CompanyDataProps {
+  id: number;
+  setCompanyData: (data: CompanyData) => void;
+  addRecruit: (recruit: RecruitData) => void;
+}
+interface ConsultDataProps {
+  id: number;
+  setConsultList: (data: ConsultData[]) => void;
+}
+
+export async function getDetailServerSideProps({
+  id,
+  setCompanyData,
+  addRecruit,
+}: CompanyDataProps) {
+  const { data, error } = await supabase
+    .from("company")
+    .select(`*, recruit (*)`) //recruit 테이블과 조인
+    .eq("id", id)
+    .single();
+  if (error) {
+    console.error("Error fetching company data:", error);
+  } else {
+    setCompanyData(data);
+    data.recruit.forEach((recruit: RecruitData) => {
+      addRecruit(recruit);
+    });
+  }
+}
+
+export async function getConsultServerSideProps({
+  id,
+  setConsultList,
+}: ConsultDataProps) {
+  const { data, error } = await supabase
+    .from("consult_company")
+    .select("*")
+    .eq("company_id", id);
+  if (error) {
+    console.error("Error fetching company data:", error);
+  } else {
+    console.log("getConsultServerSideProps에서 data: ", data);
+    setConsultList(data);
+  }
+}
