c27e6f24a2c95f1138c50e2dabbef863a4937fb5
sinchangho <vkdghckdh@gmail.com>
2023-11-05 18:31:45 +0900

    feat: 로딩페이지 기입

--- src/components/Company/Template/JobContent.tsx
+++ src/components/Company/Template/JobContent.tsx
@@ -1,34 +1,23 @@
 import NameInput from "@/components/Template/Input/NameInput";
 import StringInput from "@/components/Template/Input/StringInput";
 import LabelText from "@/components/Template/LabelText/LabelText";
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import EmployJobButton from "./EmployJobButton";
-import { RecruitData } from "../Register";
 import NumberInput from "@/components/Template/Input/NumberInput";
+import { RecruitData, initialRecruitData } from "../Interface/CompanyInterface";
+import Loading from "@/components/Loading";
 
 interface JobContentProps {
   index: number;
-  handleJobContent: (index: number, recruit: RecruitData) => void;  
+  handleJobContent: (index: number, recruit: RecruitData) => void;
   removeItem: (index: number) => void;
+  recruitData?: RecruitData;
 }
-export const initialRecruitData: RecruitData = {
-  created_at: new Date(),
-  company_id: 0,
-  job_type: "",
-  number_of_hires: "",
-  salary: "",
-  working_type: "",
-  work_start_hour: "",
-  work_end_hour: "",
-  lunch_hour: "",
-  etc: "",
-  job_availablility: false
-
-};
 function JobContent(props: JobContentProps) {
-  const { index, removeItem,  handleJobContent } = props;
+  const { index, removeItem, handleJobContent, recruitData } = props;
   const [recruit, setRecruit] = useState<RecruitData>(initialRecruitData);
-  
+
+  // console.log("recruitData: ", recruitData);
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const updatedRecruit = {
       ...recruit,
@@ -36,8 +25,13 @@ function JobContent(props: JobContentProps) {
     };
     setRecruit(updatedRecruit);
     handleJobContent(index, updatedRecruit);
-    
   };
+  //recruitData가 없으면
+  // console.log("recruitData: ", recruitData);
+  if(recruitData == undefined){
+    <Loading/>
+  }
+  else{
   return (
     <>
       <div className="mt-10 md-4">
@@ -49,7 +43,8 @@ function JobContent(props: JobContentProps) {
           <LabelText text={`구인직무${index + 1}`} />
           <NameInput
             id="job_type"
-            value={recruit.job_type}
+            value={recruitData.job_type}
+            holder={recruitData?.job_type}
             onChange={handleChange}
           ></NameInput>
         </div>
@@ -57,29 +52,50 @@ function JobContent(props: JobContentProps) {
           <LabelText text="구인인원" />
           <NumberInput
             id="number_of_hires"
-            value={recruit.number_of_hires}
+            value={recruitData.number_of_hires}
+            holder={recruitData?.number_of_hires}
             onChange={handleChange}
           ></NumberInput>
         </div>
         <div>
           {/* 급여 */}
           <LabelText text="급여" />
-          <NameInput id='salary' value={recruit.salary} onChange={handleChange}></NameInput>
+          <NameInput
+            id="salary"
+            value={recruitData.salary}
+            holder={recruitData?.salary}
+            onChange={handleChange}
+          ></NameInput>
         </div>
       </div>
       <div className="mt-6 grid grid-cols-4 gap-x-4 gap-y-6">
         <div>
           {/* 근무형태 */}
           <LabelText text="근무형태" />
-          <NameInput id='working_type' value={recruit.working_type} onChange={handleChange}></NameInput>
+          <NameInput
+            id="working_type"
+            value={recruitData.working_type}
+            holder={recruitData?.working_type}
+            onChange={handleChange}
+          ></NameInput>
         </div>
         <div>
           {/* 근무시간 */}
           <LabelText text="근무시간" />
           <div className="flex gap-3">
             {/* 시작시간, 종료시간 */}
-            <NameInput id="work_start_hour" value={recruit.work_start_hour} onChange={handleChange}></NameInput>
-            <NameInput id="work_end_hour" value={recruit.work_end_hour} onChange={handleChange}></NameInput>
+            <NameInput
+              id="work_start_hour"
+              value={recruitData.work_start_hour}
+              holder={recruitData?.work_start_hour}
+              onChange={handleChange}
+            ></NameInput>
+            <NameInput
+              id="work_end_hour"
+              value={recruitData.work_end_hour}
+              holder={recruitData?.work_end_hour}
+              onChange={handleChange}
+            ></NameInput>
           </div>
         </div>
         <div>
@@ -93,15 +109,21 @@ function JobContent(props: JobContentProps) {
         <div>
           {/* 점심시간 */}
           <LabelText text="점심시간" />
-          <NameInput id="lunch_hour" value={recruit.lunch_hour} onChange={handleChange}></NameInput>
+          <NameInput
+            id="lunch_hour"
+            value={recruitData.lunch_hour}
+            holder={recruitData?.lunch_hour}
+            onChange={handleChange}
+          ></NameInput>
         </div>
       </div>
       <div className="mt-4">
         <LabelText text="비고" />
-        <StringInput id="etc" value={recruit.etc} onChange={handleChange}/>
+        <StringInput id="etc" value={recruitData.etc} holder={recruitData?.etc} onChange={handleChange} />
       </div>
     </>
   );
+  }
 }
 
 export default JobContent;
