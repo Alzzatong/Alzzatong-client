b07618de5523e3391d07c655cdb399ac400d0a10
sinchangho <vkdghckdh@gmail.com>
2023-11-05 18:34:08 +0900

    feat: 상담내역 저장 기능 1차 구현

--- src/components/Company/Consult/index.tsx
+++ src/components/Company/Consult/index.tsx
@@ -1,18 +1,109 @@
 import RadioButton from "@/components/Template/Button/RadioButton";
 import TableList from "@/components/Template/List/TableList";
 import ChatingIcon from "../../../../public/svgs/chatingIcon";
+import { ChangeEvent, FormEvent, use, useEffect, useState } from "react";
+import { idCollection } from "../Interface/CompanyInterface";
+import NumberInput from "@/components/Template/Input/NumberInput";
+import TextAreaBox from "@/components/Template/Input/TextAreaBox";
+import { supabase } from "@/lib/supabase/supabase";
 
-const employMethods = [
-  { id: "yes", title: "본기관" },
-  { id: "no", title: "타기관" },
-  { id: "no", title: "자가 취업" },
+export const employMethods = [
+  { id: "main", title: "본기관" },
+  { id: "other", title: "타기관" },
+  { id: "self", title: "자가 취업" },
 ];
-const employStatus = [
-  { id: "yes", title: "구인중" },
-  { id: "no", title: "구인완료" },
+export const employStatus = [
+  { id: "false", title: "구인중" },
+  { id: "true", title: "구인완료" },
 ];
+export interface ConsultProps {
+  company_id: number;
+  consult_list: ConsultData[];
+}
 
-export default function CompanyConsult() {
+export interface ConsultData {
+  company_id: number;
+  created_at?: Date;
+  employ_method: string;
+  employ_status: string;
+  employ_date: string;
+  manager_name: string;
+  manager_email: string;
+  manager_phone: string;
+  content: string;
+}
+export  const consultIdCollection = {
+  company_id: "company_id",
+  employ_method: "employ_method",
+  employ_status: "employ_status",
+  employ_date: "employ_date",
+  manager_name: "manager_name",
+  manager_email: "manager_email",
+  manager_phone: "manager_phone",
+  content: "content",
+};
+export  const initialConsultData: ConsultData = {
+  company_id: 0,
+  employ_method: "",
+  employ_status: "false",
+  employ_date: "",
+  manager_name: "",
+  manager_email: "",
+  manager_phone: "",
+  content: "",
+};
+
+export default function CompanyConsult({company_id, consult_list}: ConsultProps) {
+  console.log("company_id: ", company_id);
+  console.log("CompanyConsult에서 consult_list: ", consult_list);
+  initialConsultData.company_id = company_id;
+  const [consultData, setConsultData] =
+    useState<ConsultData>(initialConsultData);
+  const [showEmployInput, setShowEmployInput] = useState(false); // 추가 입력칸 상태
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setConsultData({
+      ...consultData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleEmployRadioButtonChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setConsultData({
+      ...consultData,
+      [e.target.name]: e.target.id,
+    });
+  };
+  const handleTextAreaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setConsultData({
+      ...consultData,
+      content: e.target.value,
+    });
+  };
+
+  //저장버튼
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    const { data, error } = await supabase
+      .from("consult_company")
+      .insert([consultData]);
+    if (error) {
+      console.error("Error inserting company data:", error);
+    } else {
+      alert("저장되었습니다.");
+    }
+
+  };
+
+  useEffect(() => {
+    console.log("consultData: ", consultData);
+    if (consultData.employ_status === "true") {
+      setShowEmployInput(true);
+    } else {
+      setShowEmployInput(false);
+    }
+  }, [consultData]);
+
   return (
     <div className="bg-white">
       <h2 className="sr-only">상담내역</h2>
@@ -25,7 +116,8 @@ export default function CompanyConsult() {
             </div>
             <RadioButton
               itemList={employMethods}
-              groupName={"구인방법"}
+              groupName={consultIdCollection.employ_method}
+              onChange={handleEmployRadioButtonChange}
             ></RadioButton>
           </div>
           <div className="mt-6 flex  gap-x-4 gap-y-6">
@@ -34,8 +126,19 @@ export default function CompanyConsult() {
             </div>
             <RadioButton
               itemList={employStatus}
-              groupName={"구인상태"}
+              groupName={consultIdCollection.employ_status}
+              onChange={handleEmployRadioButtonChange}
             ></RadioButton>
+            {showEmployInput && (
+              <div className="mt-1 ml-3">
+                <NumberInput
+                  id={consultIdCollection.employ_date}
+                  value={consultData.employ_date}
+                  holder="구인일짜"
+                  onChange={handleChange}
+                ></NumberInput>
+              </div>
+            )}
           </div>
         </div>
       </div>
@@ -44,11 +147,17 @@ export default function CompanyConsult() {
         <ChatingIcon className="w-10 h-10" />
         <div>상담내용</div>
       </div>
-      <div className="mt-2 w-100% h-44 bg-white rounded-md border border-neutral-200" />
+      <TextAreaBox
+        id={consultIdCollection.content}
+        value={consultData.content}
+        holder="상담내용을 입력해주세요."
+        onChange={handleTextAreaChange}
+      ></TextAreaBox>
       <div className="flex justify-end  sm:ml-16 sm:mt-0 sm:flex-none">
         <button
           type="button"
           className="mt-4 block rounded-md bg-blue-500 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          onClick={handleSubmit}
         >
           저장
         </button>
--- src/components/Company/Interface/CompanyInterface.tsx
+++ src/components/Company/Interface/CompanyInterface.tsx
@@ -1,3 +1,26 @@
+// id모음
+export const idCollection = {
+  businessYearId: "business_year",
+  businessNumberId: "business_number",
+  companyNameId: "company_name",
+  ceoNameId: "ceo_name",
+  businessCodeId: "business_code",
+  mainPhoneId: "main_phone",
+  secondPhoneId: "second_phone",
+  faxId: "fax",
+  addressId: "address",
+  regionId: "region",
+  localDetailId: "local_detail",
+  foundationYearId: "foundation_year",
+  typeId: "type",
+  isInsuranceId: "is_insurance",
+  managerNameId: "manager_name",
+  managerEmailId: "manager_email",
+  managerPhoneId: "manager_phone",
+  certificationLinkId: "certification_link",
+  contentId: "content",
+};
+
 export const fourinsureMethods = [
   { id: "true", title: "가입" },
   { id: "false", title: "미가입" },
@@ -29,49 +52,154 @@ export interface CompanyData {
 
 // 초기 상태 정의
 export const initialCompanyData: CompanyData = {
-    // 초기 상태 정의
-    created_at: new Date(),
-    business_year: "",
-    business_number: "",
-    company_name: "",
-    ceo_name: "",
-    business_code: "",
-    main_phone: "",
-    second_phone: "",
-    fax: "",
-    address: "",
-    region: "",
-    local_detail: "",
-    foundation_year: "",
-    type: "",
-    is_insurance: false,
-    manager_name: "",
-    manager_email: "",
-    manager_phone: "",
-    certification_link: "",
-    content: "",
-    // employee_list: [],
-  };
+  created_at: new Date(),
+  business_year: "",
+  business_number: "",
+  company_name: "",
+  ceo_name: "",
+  business_code: "",
+  main_phone: "",
+  second_phone: "",
+  fax: "",
+  address: "",
+  region: "",
+  local_detail: "",
+  foundation_year: "",
+  type: "",
+  is_insurance: false,
+  manager_name: "",
+  manager_email: "",
+  manager_phone: "",
+  certification_link: "",
+  content: "",
+  // employee_list: [],
+};
 
-  // id모음 
-  export const idCollection = {
-    businessYearId: "business_year",
-    businessNumberId: "business_number",
-    companyNameId: "company_name",
-    ceoNameId: "ceo_name",
-    businessCodeId: "business_code",
-    mainPhoneId: "main_phone",
-    secondPhoneId: "second_phone",
-    faxId: "fax",
-    addressId: "address",
-    regionId: "region",
-    localDetailId: "local_detail",
-    foundationYearId: "foundation_year",
-    typeId: "type",
-    isInsuranceId: "is_insurance",
-    managerNameId: "manager_name",
-    managerEmailId: "manager_email",
-    managerPhoneId: "manager_phone",
-    certificationLinkId: "certification_link",
-    contentId: "content",
-  };
+// 구인정보
+export interface RecruitData {
+  created_at: Date;
+  company_id: number;
+  job_type: string;
+  number_of_hires: string;
+  salary: string;
+  working_type: string;
+  work_start_hour: string;
+  work_end_hour: string;
+  lunch_hour: string;
+  job_availablility: boolean;
+  etc: string;
+}
+export const initialRecruitData: RecruitData = {
+  created_at: new Date(),
+  company_id: 0,
+  job_type: "",
+  number_of_hires: "",
+  salary: "",
+  working_type: "",
+  work_start_hour: "",
+  work_end_hour: "",
+  lunch_hour: "",
+  etc: "",
+  job_availablility: false,
+};
+
+// 조인테이블
+export interface JoinData {
+  created_at: Date;
+  business_year: string;
+  business_number: string;
+  company_name: string;
+  ceo_name: string;
+  business_code: string;
+  main_phone: string;
+  second_phone: string;
+  fax: string;
+  address: string;
+  region: string;
+  local_detail: string;
+  foundation_year: string;
+  type: string;
+  is_insurance: boolean;
+  manager_name: string;
+  manager_email: string;
+  manager_phone: string;
+  certification_link: string;
+  content: string;
+  recruit: RecruitData[];
+}
+
+export const initialJoinData: JoinData = {
+  created_at: new Date(),
+  business_year: "",
+  business_number: "",
+  company_name: "",
+  ceo_name: "",
+  business_code: "",
+  main_phone: "",
+  second_phone: "",
+  fax: "",
+  address: "",
+  region: "",
+  local_detail: "",
+  foundation_year: "",
+  type: "",
+  is_insurance: false,
+  manager_name: "",
+  manager_email: "",
+  manager_phone: "",
+  certification_link: "",
+  content: "",
+  recruit: [], // 초기화용 RecruitData 배열을 추가해야 할 수도 있습니다.
+};
+
+export const dummy: JoinData = {
+  business_year: "2021",
+  business_number: "123456789",
+  company_name: "우리회사",
+  ceo_name: "김대표",
+  business_code: "123456",
+  main_phone: "01012345678",
+  fax: "01012345678",
+  manager_name: "홍길동",
+  manager_email: "manager@gmail.com",
+  address: "서울시 강남구",
+  foundation_year: "2021",
+  is_insurance: true,
+  content: "입력한 아무내용이 노출됩니다.",
+  created_at: new Date(),
+  second_phone: "",
+  region: "",
+  local_detail: "",
+  type: "",
+  manager_phone: "",
+  certification_link: "",
+  recruit: [
+    {
+      created_at: new Date(),
+      company_id: 1,
+      job_type: "테스트",
+      number_of_hires: "1",
+      salary: "200",
+      working_type: "주 5일",
+      work_start_hour: "9",
+      work_end_hour: "18",
+      lunch_hour: "12",
+      job_availablility: true,
+      etc: "비고 입력란, 점심미지급 등 입력한 아무내용이 노출됩니다.",
+    },
+    {
+      created_at: new Date(),
+      company_id: 2,
+      job_type: "테스트2",
+      number_of_hires: "2",
+      salary: "300",
+      working_type: "주 5일",
+      work_start_hour: "9",
+      work_end_hour: "18",
+      lunch_hour: "12",
+      job_availablility: true,
+      etc: "비고 입력란, 점심미지급 등 입력한 아무내용이 노출됩니다.",
+    },
+  ],
+
+};
