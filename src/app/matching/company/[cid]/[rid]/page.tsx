73a5cf4f743a8e5aac6849aed5052f9d3b53a903
sinchangho <vkdghckdh@gmail.com>
2023-11-11 19:12:53 +0900

    fix: 무한 렌더링 해결

--- src/app/matching/company/[cid]/[rid]/page.tsx
+++ src/app/matching/company/[cid]/[rid]/page.tsx
@@ -31,67 +31,63 @@ export default function CompanyMatchingRecruitsSeniorPage(props: any) {
   // 조회한 값으로 하위 컴포넌트에 전달
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true);
       getRegionAndLocationJobTypeConsultServerSideProps({
         company_id: company_id,
         recruit_id: recruit_id,
         setRegionLocation: setRegionLocation,
         setJobType: setJobType,
       });
-      setIsLoading(false);
     };
     fetchData();
-  }, [jobType]);
+  }, [company_id, recruit_id]);
+  useEffect(() => {
+    getServerSideCompanyName({
+      id: company_id,
+      setCompanyName: setCompanyName,
+      setAddress: setAddress,
+    });
 
-  getServerSideCompanyName({
-    id: company_id,
-    setCompanyName: setCompanyName,
-    setAddress: setAddress,
-  });
+    getServerSideRecruit({
+      recruit_id: recruit_id,
+      setRecruit: setRecruit,
+    });
+  }, []);
 
-  getServerSideRecruit({
-    recruit_id: recruit_id,
-    setRecruit: setRecruit,
-  });
-
-  if (isLoading) {
-    return <Loading />;
-  } else {
-    return (
-      <div className="bg-gray-50">
-        <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h1 className="text-4xl	font-bold text-gray-900">구인처 매칭</h1>
-          <div className="mx-2 mt-11 grid grid-cols-2 gap-2 mb-3">
-            <div className="flex justify-start gap-2">
-              <div className=" text-black text-xl font-medium font-['Pretendard'] leading-7">
-                회사명 |
-              </div>
-              <div className="text-black text-xl font-medium font-['Pretendard'] leading-7">
-                {companyName}
-              </div>
+  return (
+    <div className="bg-gray-50">
+      <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h1 className="text-4xl	font-bold text-gray-900">구인처 매칭</h1>
+        <div className="mx-2 mt-11 grid grid-cols-2 gap-2 mb-3">
+          <div className="flex justify-start gap-2">
+            <div className=" text-black text-xl font-medium font-['Pretendard'] leading-7">
+              회사명 |
             </div>
-            <div className="flex justify-end gap-2">
-              <div className="mt-2 text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
-                주소
-              </div>
-              <div className="mt-2 text-black text-sm font-medium font-['Pretendard'] leading-tight">
-                {address}
-              </div>
+            <div className="text-black text-xl font-medium font-['Pretendard'] leading-7">
+              {companyName}
             </div>
           </div>
-          {recruit && 
-          <Link href={`/matching/company/${company_id}`}>
-          <RecruitBox recruit={recruit}/>
-          </Link>}
-          <MatchingTabRecuruitSeniorSearch
-            company_id={company_id}
-            recruit_id={recruit_id}
-            region={region}
-            location={location}
-            job_type={jobType}
-          />
+          <div className="flex justify-end gap-2">
+            <div className="mt-2 text-zinc-500 text-sm font-medium font-['Pretendard'] leading-tight">
+              주소
+            </div>
+            <div className="mt-2 text-black text-sm font-medium font-['Pretendard'] leading-tight">
+              {address}
+            </div>
+          </div>
         </div>
+        {recruit && (
+          <Link href={`/matching/company/${company_id}`}>
+            <RecruitBox recruit={recruit} />
+          </Link>
+        )}
+        <MatchingTabRecuruitSeniorSearch
+          company_id={company_id}
+          recruit_id={recruit_id}
+          region={region}
+          location={location}
+          job_type={jobType}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
--- src/components/Matching/Search/MatchingTabRecuruitSeniorSearch.tsx
+++ src/components/Matching/Search/MatchingTabRecuruitSeniorSearch.tsx
@@ -9,6 +9,8 @@ import { SeniorData } from "@/components/Senior/Interface/SeniorInterface";
 import SeniorBox from "../SelectBox/SeniorBox";
 import { get } from "http";
 import { getSeniorInfoList } from "@/services/supabase/matchingCompanyAPI";
+import React from "react";
+import Loading from "@/components/Loading";
 
 interface MatchingTabRecuruitSeniorSearchProps {
   company_id: number;
@@ -39,87 +41,62 @@ export default function MatchingTabRecuruitSeniorSearch({
   job_type,
 }: MatchingTabRecuruitSeniorSearchProps) {
   const [seniors, setSeniors] = useState<SeniorBoxType[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  // // 희망 근무지와 희망 직종에 맞는 구직자조회
-  // const handleSeniorSearch = async () => {
-  //   let { data, error } = await supabase
-  //     .from("senior_wishlist")
-  //     .select(`senior_id`)
-  //     .eq("location", region)
-  //     .eq("location_detail", location)
-  //     .eq("job_type_name", job_type);
-
-  //   if (error) console.error("Error loading data: ", error);
-  //   else {
-  //     if (data && data.length > 0) {
-  //       let tempSenior = []; // 임시 배열 생성
-  //       console.log("data: ", data);
-  //       for (let i = 0; i < data.length; i++) {
-  //         let { data: seniorData, error: seniorError } = await supabase
-  //           .from("senior")
-  //           .select(`*`)
-  //           .eq("senior_id", data[i].senior_id)
-  //           .single();
-  //         if (seniorError) console.error("Error loading data: ", seniorError);
-  //         else {
-  //           console.log("seniorData: ", seniorData);
-  //           tempSenior.push(seniorData);
-  //         }
-  //       }
-  //       setSeniors(tempSenior);
-  //     }
-  //   }
-  // };
-
+  const fetchData = async () => {
+    if (region && location && job_type) {
+      await getSeniorInfoList({ region, location, job_type, setSeniors });
+      setIsLoading(false);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      if (seniors.length < 1) { // 무한 렌더링 방지
-        await getSeniorInfoList({ region, location, job_type, setSeniors });
-      }
-    };
     fetchData();
-  }, [job_type]);
+  }, [region, location, job_type]);
 
-  return (
-    <div>
-      <h2 className="mt-10 text-l	font-semibold text-gray-900">
-        검색 결과
-        <span className="text-xl  text-blue-500 text-l font-bold font-['Pretendard'] leading-tight">
-          {seniors?.length}
-        </span>
-      </h2>
-      <div className="min-h-[560px]">
-        <ul
-          role="list"
-          className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-1"
-        >
-          {seniors &&
-            seniors.map((senior: SeniorBoxType) => {
-              let dateObject = new Date(senior.created_at);
-              let dateString = dateObject.toISOString();
-              let date = dateString.slice(0, 10);
-              return (
-                <li
-                  key={senior.senior_id}
-                  className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
-                >
-                  <Link
-                    href={`/matching/company/${company_id}/${recruit_id}/${senior.senior_id}`}
+  if (isLoading) {
+    <Loading />;
+  } else {
+    return (
+      <div>
+        <h2 className="mt-10 text-l	font-semibold text-gray-900">
+          검색 결과
+          <span className="text-xl  text-blue-500 text-l font-bold font-['Pretendard'] leading-tight">
+            {seniors?.length}
+          </span>
+        </h2>
+        <div className="min-h-[560px]">
+          <ul
+            role="list"
+            className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-1"
+          >
+            {seniors &&
+              seniors.map((senior: SeniorBoxType) => {
+                let dateObject = new Date(senior.created_at);
+                let dateString = dateObject.toISOString();
+                let date = dateString.slice(0, 10);
+                return (
+                  <li
+                    key={senior.senior_id}
+                    className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
                   >
-                    {/* //컴포넌트화 해야할 부분 */}
-                    <SeniorBox
-                      senior={senior}
-                      date={date}
-                      job_type={job_type}
-                      region={region}
-                      location={location}
-                    />
-                  </Link>
-                </li>
-              );
-            })}
-        </ul>
+                    <Link
+                      href={`/matching/company/${company_id}/${recruit_id}/${senior.senior_id}`}
+                    >
+                      {/* //컴포넌트화 해야할 부분 */}
+                      <SeniorBox
+                        senior={senior}
+                        date={date}
+                        job_type={job_type}
+                        region={region}
+                        location={location}
+                      />
+                    </Link>
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
