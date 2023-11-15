0150ef73b3c6e98eb6ff5716c827338a4b8519a6
yueyui <ouyui1990@gmail.com>
2023-11-15 13:40:08 +0900

    app의 시니어 layout에서 디테일과 상담페이지로의 데이터 전달을 위한 코드 추가

--- src/app/senior/[sid]/layout.tsx
+++ src/app/senior/[sid]/layout.tsx
@@ -1,16 +1,46 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SeniorDetail from "@/components/Senior/Detail";
 import SeniorConsult from "@/components/Senior/Consult";
 import TabNavigation from "@/components/Senior/TabNav/TabNavigation";
-
+import BigTitle from "@/components/Template/LabelText/BigTitle";
+import { SeniorConsultJoinAdmin, SeniorJoinWishCareer } from "@/components/Senior/Interface/SeniorInterface";
+import { getSeniorConsultSelect, getSeniorWishCareerSelect } from "@/queries/SelectSenior";
+import { ConvertToIdType } from "@/components/Senior/Template/ConvertToIdType";
 
 interface IdParams {
-  id: number;
+  sid: number;
+}
+interface IdType {
+  children: React.ReactNode;
+  params: IdParams;
+}
+interface sidProps {
+  sid: IdType;
 }
 
-export default function DetailPage(props: any) {
+export default function DetailPage(sid: IdType) {
   const [currentTab, setCurrentTab] = useState("구직자 상세조회");
+  const [joinData, setJoinData] = useState<SeniorJoinWishCareer[] | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [consultField, setConsultField] = useState<SeniorConsultJoinAdmin[] | undefined>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    getSeniorWishCareerSelect(sid.params.sid).then(data => setJoinData(data));
+    getSeniorConsultSelect(sid.params.sid).then(data => setConsultField(data));
+    setIsLoading(false);
+  }, [sid]);
+
+  if (isLoading) {
+    console.log("isLoading");
+    return <div>Loading...</div>;
+  }
+
+  // const idTypeData = ConvertToIdType(joinData);
+  // if (!idTypeData) {
+  //   return null;
+  // }
 
   function handleClick(name: string) {
     setCurrentTab(name);
@@ -18,9 +48,32 @@ export default function DetailPage(props: any) {
 
   return (
     <div>
-      <TabNavigation handleClick={handleClick} currentTab={currentTab} />
-      <div>
-        {currentTab == "구직자 상세조회" ? <SeniorDetail sid={props} /> : <SeniorConsult />}
+      <div className="w-full relative bg-white">
+        <div className="mx-auto max-w-4xl">
+          <div className="sidebarContent"></div>
+          <div className="whiteSpace" />
+          <TabNavigation handleClick={handleClick} currentTab={currentTab} />
+          <div className="pageContent mt-8">
+          {currentTab == "구직자 상세조회" ? (
+            <main>
+              <BigTitle
+                text="구직자 상세조회"
+                className="text-4xl font-bold text-gray-900"
+              />
+              {joinData && <SeniorDetail sidData={joinData}></SeniorDetail>}
+            </main>
+          ) : (
+            <main>
+              <BigTitle
+                text="상담내역"
+                className="text-4xl font-bold text-gray-900"
+              />
+              {consultField && <SeniorConsult sidData={consultField}></SeniorConsult>}
+            </main>
+          )}
+          </div>
+          <div className="sidebarContent"></div>
+        </div>
       </div>
     </div>
   );
