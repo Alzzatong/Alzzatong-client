33ddde81ec4117b4ead5f22d86f1da367e519c3e
yueyui <ouyui1990@gmail.com>
2023-11-17 00:18:40 +0900

    진짜 중요한 화면 배경화면색 가득 채우기!!!

--- src/app/matching/senior/[sid]/page.tsx
+++ src/app/matching/senior/[sid]/page.tsx
@@ -43,7 +43,7 @@ export default function SeniorMatchingCampanyPage(props: any) {
   };
 
   return (
-    <div className="bg-gray-50" style={{ maxHeight: "800px" }}>
+    <div className="bg-gray-50">
       <div className="mx-auto max-w-2xl px-4  pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
         {/* <BigTitle
           text="구직자 매칭"
