dc96c63615faa18883a22e214e3a4e755f549a8a
yueyui <ouyui1990@gmail.com>
2023-11-16 23:58:38 +0900

    디자인 수정

--- src/components/Template/SelectBox/UncontrolledDropbox.tsx
+++ src/components/Template/SelectBox/UncontrolledDropbox.tsx
@@ -16,7 +16,7 @@ export default function UncontrolledDropbox({ id, itemList, groupName, defaultVa
   }
 
   return (
-    <div>
+    <div className="mt-1">
       <legend className="sr-only">{groupName}</legend>
       <label
 
@@ -28,7 +28,7 @@ export default function UncontrolledDropbox({ id, itemList, groupName, defaultVa
         name={id}
         defaultValue={defaultValue}
         onBlur={handleSelect}
-        className="mt-2 block w-full bg-white rounded-md shadow-sm border-0 p-2 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        className="block w-full bg-white rounded-md shadow-sm border-0 p-2 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-400 focus:ring-2 focus:border-indigo-500 focus:ring-indigo-600 sm:text-sm sm:leading-6"
       >
         {itemList.map((item:any) => 
           <option key={item.id} value={item.id}>
